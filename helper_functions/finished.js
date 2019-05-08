3436d98f5d93c1b61d96a9c408ee954beb522d5d
kaoslabs <42324929+kaoslabs@users.noreply.github.com>
2019-05-08 12:16:47 -0500

    create finished.js add finish function and TODO

--- helper_functions/finished.js
+++ helper_functions/finished.js
@@ -1,5 +1,6 @@
-// TODO: break all recursive functions (propogations)
+import {exportGrid} from './gridFunctions';
 
 function finish(grid){
-    
+    // TODO: break all recursive functions (propogations)
+    exportGrid(grid);
 }
