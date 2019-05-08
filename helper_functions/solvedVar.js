e54e2325efe4e5c48998077ab31324423fbc2412
kaoslabs <42324929+kaoslabs@users.noreply.github.com>
2019-05-08 11:09:44 -0500

    create solved value and functions for accessing it

---
+++ helper_functions/solvedVar.js
@@ -0,0 +1,17 @@
+// amount of solved cells
+var solved;
+
+// set new value to solved cells
+function setSolved(num){
+    solved = num;
+}
+
+// return value of solved cells
+function getSolved(){
+    return solved;
+}
+
+// increment value of solved cells up by one
+function addSolved(){
+    solved++;
+}
