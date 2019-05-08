aabb19935f177681b9609b7de5d592e2e801e96c
kaoslabs <42324929+kaoslabs@users.noreply.github.com>
2019-05-08 11:23:41 -0500

    organize propograte functions into one file move propogate to helper functions

---
+++ helper_functions/propogate.js
@@ -0,0 +1,15 @@
+function propogateGrid(row, col, values){
+    
+}
+
+function propogateRow(row, values){
+
+}
+
+function propogateCol(col, values){
+    
+}
+
+function propogateBox(row, col, values){
+    
+}
