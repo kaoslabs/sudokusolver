0cd95faff16218995eebc80f7212ab0241701789
kaoslabs <42324929+kaoslabs@users.noreply.github.com>
2019-05-17 16:01:37 -0500

    add get...Values to return array of values update propogate to have optional ignore parameter write code for finding pairs

--- helper_functions/gridFunctions.js
+++ helper_functions/gridFunctions.js
@@ -37,6 +37,36 @@ function Grid(){
         // TODO: make this work! app.export(grid_arr)
     }
 
+    // returns array of all values in given row
+    this.getRowValues = function(row){
+        let arr = [];
+        for (c = 0; c < 9; c++){
+            arr.push(cells[row][c].values);
+        }
+        return arr;
+    }
+
+    // returns array of all values in given column
+    this.getColValues = function(col){
+        let arr = [];
+        for (r = 0; r < 9; r++){
+            arr.push(cells[r][col].values);
+        }
+        return arr;
+    }
+
+    // returns array of all values in given box
+    this.getBoxValues = function(row, col){
+        row_box = defineBox(row);
+        col_box = defineBox(col);
+        let arr = [];
+        for (r = row_box; r < row_box + 3; r++){
+            for (c = col_box; c < col_box + 3; c++){
+                arr.push(cells[r][c].values);
+            }
+        }
+        return arr;
+    }
 }
 
 // defines the start value of grid row or column
--- helper_functions/solverFunctions.js
+++ helper_functions/solverFunctions.js
@@ -1,4 +1,4 @@
-import {defineBox} from './gridFunctions';
+import {Grid, defineBox} from './gridFunctions';
 import {Cell} from './cellFunctions';
 
 function Solver(){
@@ -22,10 +22,17 @@ function Solver(){
     }
 
     // remove values from cells in row
-    this.propogateRow = function(row, values){
+    // xcols parameter will instead set values for given columns
+    this.propogateRow = function(row, values, xcols){
+        if (!xcols) xcols = [];
         // iterate cells across row
         for (let c = 0; c < 9; c++){
             let cell = grid.cells[row][c];
+            // set cells in given columns to values
+            if(xcols.includes(c)){
+                cell.setCell(values);
+                continue;
+            }
             // only remove values from cell IF cell is still an array
             if (!cell.solved){
                 cell.removeValues(values);
@@ -34,10 +41,17 @@ function Solver(){
     }
 
     // remove values from cells in column
-    this.propogateCol = function(col, values){
+    // xrows parameter will instead set values for given rows
+    this.propogateCol = function(col, values, xrows){
+        if (!xrows) xrows = [];
         // iterate cells down column
         for (let r = 0; r < 9; r++){
             let cell = grid.cells[r][col];
+            // set cells in given rows to values
+            if(xrows.includes(r)){
+                cell.setCell(values);
+                continue;
+            }
             // only remove values from cell IF cell is still an array
             if (!cell.solved){
                 cell.removeValues(values);
@@ -46,13 +60,20 @@ function Solver(){
     }
 
     // remove values from cells in box
-    this.propogateBox = function(row, col, values){
+    // xcells parameter will instead set values for given rows & columns
+    // xcells must be passed in as an object array [{row: r, col: c}, ...]
+    this.propogateBox = function(row, col, values, xcells){
+        if (!xcells) xcells = [];
         let row_box = defineBox(row);
         let col_box = defineBox(col);
         // iterate cells in box
         for (let r = row_box; r < row_box + 3; r++){
             for (let c = col_box; c < col_box + 3; c++){
                 let cell = grid.cells[r][c];
+                if (xcells.includes({row: r, col: c})){
+                    cell.setCell(values);
+                    continue;
+                }
                 // only remove values from cell IF cell is not solved
                 if (!cell.solved){
                     cell.removeValues(values);
@@ -117,9 +138,9 @@ function Solver(){
     }
 
     // check for solo values in boxes
-    this.findSolosBox = function(row_box, col_box){
-        // row_box = defineBox(row);
-        // col_box = defineBox(col);
+    this.findSolosBox = function(row, col){
+        let row_box = defineBox(row);
+        let col_box = defineBox(col);
         for (i = 1; i <= 9; i++){
             let count = 0;
             let row = 0;
@@ -139,6 +160,84 @@ function Solver(){
             // note: setCell will then propogate
             if (count == 1){
                 grid.cells[row][col].setCell(i);
+            }
+        }
+
+        // begins to look for pairs across rows, columns, boxes
+        this.findPairs = function(){
+            for (let r = 0; r < 9; r++){
+                findPairsRow(r);
+            }
+            for (let c = 0; c < 9; c++){
+                findPairsCol(c);
+            }
+            for (let r = 0; r < 9; r+= 3){
+                for (let c = 0; c < 9; c+= 3){
+                    findPairsBox(r, c);
+                }
+            }
+        }
+
+        // searches row for pairs and propogates
+        this.findPairsRow = function(row){
+            // TODO: FIX includes! Needs an index for second cell!!
+            let row_values = grid.getRowValues;
+            for (let c = 0; c < 9; c++){
+                let values = grid.cells[row][c].values;
+                if(row_values.includes(values, c + 1)){
+                    let xcols = [c, twofers[j].col];
+                    propogateRow(row, twofers[i].values, xcols);
+                }
+            }
+        }
+
+        this.findPairsCol = function(col){
+            let twofers = [];
+            for (let r = 0; r < 9; r++){
+                let values = grid.cells[row][c].values;
+                if (values.length == 2) twofers.push({values: values, row: r});
+            }
+            if (twofers.length >= 2){
+                for (let i = 0; i < twofers.length - 1; i++){
+                    for (let j = i + 1; j < twofers.length; j++){
+                        if (twofers[i].values == twofers[j].values){
+                            let xrows = [twofers[i].row, twofers[j].row];
+                            propogateCol(col, twofers[i].values, xrows);
+                        }
+                    }
+                }
+            }
+            
+        }
+
+        this.findPairsBox = function(row, col){
+            let twofers = [];
+            let row_box = defineBox(row);
+            let col_box = defineBox(col);
+            for (let r = row_box; r < row_box + 3; r++){
+                for (let c = col_box; c < col_box + 3; c++){
+                    let values = grid.cells[r][c].values;
+                    if (values.length == 2) twofers.push({values: values, row: r, col: c});
+                }
+                if (twofers.length >= 2){
+                    for (let i = 0; i < twofers.length - 1; i++){
+                        for (let j = i + 1; j < twofers.length; j++){
+                            if (twofers[i].values == twofers[j].values){
+                                let xcells = [
+                                                {
+                                                    row: twofers[i].row,
+                                                    col: twofers[i].col
+                                                },
+                                                {
+                                                    row: twofers[j].row,
+                                                    col: twofers[j].col
+                                                }
+                                            ];
+                                propogateBox(row, col, values, xcells);
+                            }
+                        }
+                    }
+                }
             }
         }
     }
